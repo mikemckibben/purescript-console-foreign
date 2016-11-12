d3f026f1a808f644eafb4dff32b91cea1a946dfd
Michael McKibben <mikemckibben@gmail.com>
2016-11-12 10:22:37 -0700

    rename log => logF, info => infoF, warn => warnF, error => errorF so name clashing doesn't occur with standard Console module

--- src/Control/Monad/Eff/Console/Foreign.purs
+++ src/Control/Monad/Eff/Console/Foreign.purs
@@ -1,10 +1,10 @@
 --| Support for logging native javascript objects to the console using
 --| the `AsForeign` class.
 module Control.Monad.Eff.Console.Foreign
-       ( log
-       , info
-       , warn
-       , error) where
+       ( logF
+       , infoF
+       , warnF
+       , errorF) where
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE)
@@ -14,20 +14,20 @@ import Data.Foreign.Class (class AsForeign, write)
 import Data.Unit (Unit)
 
 -- | Write a message to the native console using `console.log`.
-log :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
-log = _log <<< write
+logF :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
+logF = _log <<< write
 
 -- | Write an info message to the native console using `console.info`.
-info :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
-info = _info <<< write
+infoF :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
+infoF = _info <<< write
 
 -- | Write a warning message to the native console using `console.warn`.
-warn :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
-warn = _warn <<< write
+warnF :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
+warnF = _warn <<< write
 
 -- | Write an error message to the native console using `console.error`.
-error :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
-error = _error <<< write
+errorF :: forall a e. AsForeign a => a -> Eff (console :: CONSOLE | e) Unit
+errorF = _error <<< write
 
 foreign import _log :: forall e. Foreign -> Eff (console :: CONSOLE | e) Unit
 foreign import _info :: forall e. Foreign -> Eff (console :: CONSOLE | e) Unit
